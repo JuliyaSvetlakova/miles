1243329f07e37dd6afc6158ef5ffff6eb3650057
Juliya Svetlakova <9268385539@mail.ru>
2023-05-31 08:01:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2050;
+        int oneMile = 20;
+        int bonus = (amount / oneMile);
+        System.out.println("Мили: " + bonus);
+
+    }
+}
